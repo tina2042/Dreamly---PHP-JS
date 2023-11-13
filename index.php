9ddaafb7827b79aa69a06526feae44c2f54f51b6
tina2042 <tina2042@gmail.com>
2023-11-13 18:37:18 +0100

    lab4

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+require_once 'Routing.php';
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
 
-echo 'Hi there 👋';
+Routing::get('', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('dashboard', 'DefaultController');
+
+Routing::run($path);
 
 ?>
